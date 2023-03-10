ff1c282287ef4c2bbd31de22cab718106ac4a785
josivcoe <josivcoe@gmail.com>
2023-03-10 14:52:54 -0500

    canvas size

---
+++ game.js
@@ -0,0 +1,34 @@
+const canvas = document.querySelector("#game");
+const game = canvas.getContext('2d');
+
+window.addEventListener('load', startGame);
+
+function startGame(){
+    let canvasSize;
+    if (window.innerHeight > window.innerWidth){
+        canvasSize = window.innerWidth * 0.8;
+    } else {
+        canvasSize = window.innerHeight * 0.8;
+    }
+
+    canvas.setAttribute('width', canvasSize);
+    canvas.setAttribute('height', canvasSize);
+
+    const elementsSize = canvasSize /10;
+    console.log({canvasSize, elementsSize});
+    
+    game.font = elementsSize + 'px Verdana';
+    game.textAlign = 'end';
+    for (let i = 1; i <= 10; i++){
+        game.fillText(emojis['X'], elementsSize,elementsSize*i);
+    }
+
+    
+    
+    // game.fillRect(100,25,100,100);
+    // game.clearRect(125,50,50,50);
+    // game.font = '25px verdana'
+    // game.fillStyle = 'purple';
+    // game.textAlign = 'left'
+    // game.fillText('PLATZI', 25,25);
+}

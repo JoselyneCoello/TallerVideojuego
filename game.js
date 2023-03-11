30eedce110e7b7bb5f994f15bb17cb71d3ab5d26
josivcoe <josivcoe@gmail.com>
2023-03-10 21:27:37 -0500

    eventos y botones

--- game.js
+++ game.js
@@ -1,5 +1,10 @@
 const canvas = document.querySelector("#game");
 const game = canvas.getContext('2d');
+const btnUp = document.querySelector('#up');
+const btnLeft = document.querySelector('#left');
+const btnRight = document.querySelector('#right');
+const btnDown = document.querySelector('#down');
+
 let canvasSize
 let elementsSize;
 
@@ -50,4 +55,32 @@ function startGame(){
     //game.fillStyle = 'purple';
     // game.textAlign = 'left'
     // game.fillText('PLATZI', 25,25);
+}
+window.addEventListener('keydown', moveByKeys);
+btnUp.addEventListener('click', moveUp);
+btnLeft.addEventListener('click', moveLeft);
+btnRight.addEventListener('click', moveRight);
+btnDown.addEventListener('click', moveDown);
+
+function moveByKeys(event){
+    if (event.key == 'ArrowUp')
+        moveUp();
+    else if ( event.key == 'ArrowLeft')
+        moveLeft();
+    else if ( event.key == 'ArrowRight')
+        moveRight();
+    else if ( event.key == 'ArrowDown')
+        moveDown();
+}
+function moveUp(){
+
+}
+function moveLeft(){
+    
+}
+function moveRight(){
+    
+}
+function moveDown(){
+    
 }
